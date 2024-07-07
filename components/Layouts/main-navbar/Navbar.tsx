5e9344c1eccf44768b863b60a3d63662339d236a
david temitope <temi236topemicheal@gmail.com>
2024-07-07 12:37:44 +0100

    fix: adjusting the header nav vertically

--- components/Layouts/main-navbar/Navbar.tsx
+++ components/Layouts/main-navbar/Navbar.tsx
@@ -21,12 +21,12 @@ export const Navbar = () => {
           <h1 className="text-header-3 font-clash-display">Avion</h1>
         </Link>
 
-        <div className="hidden gap-x-4 lg:flex">
+        <div className="hidden gap-x-4 lg:flex items-center">
           <ShoppingCart className="size-4 cursor-pointer lg:size-6" />
           <UserAvatar className="size-4 cursor-pointer lg:size-6" />
         </div>
 
-        <div className="flex gap-x-4 lg:hidden">
+        <div className="flex gap-x-4 lg:hidden items-center">
           <Search className="size-4 cursor-pointer lg:size-6" />
           <Menu className="cursor-pointer lg:size-6" />
         </div>
